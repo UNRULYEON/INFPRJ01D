be71b370b41a02f6a47eb18248d86396fb8ef0fb
Amar Kisoensingh <amar.kisoensingh@gmail.com>
2019-05-13 09:48:19 +0200

    Created navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 // Components
-import Header from './components/header'
+import Header from './components/header';
+import Navbar from './components/navbar';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <Navbar />
     </div>
   );
 }
