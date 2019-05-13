d1eb315ab1e25e8a6f1f2e8fafd01f1f68980264
Amar Kisoensingh <amar.kisoensingh@gmail.com>
2019-05-13 09:28:35 +0200

    Added header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header: React.FC = () => {
+  return (
+    <div className="Header--container">
+      <span id="Header--company-name">DEPT</span>
+    </div>
+  );
+}
+
+export default Header;
