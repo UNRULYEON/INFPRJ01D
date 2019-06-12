ea06521f4d6330f66e111195a94ba1c08161d21b
vphr <0956458@hr.nl>
2019-06-13 01:19:00 +0200

    fixed product and sales endpoint

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('products', views.SalesAll.as_view()),
-    path('products/predict/<int:pk>', views.predict),
+    path('products', views.ProductsAll.as_view()),
+    path('sales', views.SalesAll.as_view())
+    path('sales/predict/<int:pk>', views.predict),
     #path('sales', views.GetProduct.as_view()),
     #path('sales/predict/<int:pk>', views.predict),
     #path('sales/test', views.SalesAll.as_view())
